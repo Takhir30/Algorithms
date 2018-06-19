37f3c96fd0b7e6f19fa15b8ec5aa9f3873d8a82c
Takhir <baygeriev87@mail.ru>
2018-06-19 20:16:12 +0300

    added bubblesort

--- algorithms.py
+++ algorithms.py
@@ -2,6 +2,16 @@ import time
 import random
 
 
+def bubblesort(array):
+    m = len(array)-1
+    for j in range(len(array)-1):
+        for i in range(m):
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+        m -= 1
+    return array
+
+
 def quicksort(array):
     pivot = array[-1]
     index_pivot = len(array) - 1
@@ -33,8 +43,22 @@ def quicksort(array):
         return quicksort(array[:index_pivot]) + [pivot] + quicksort(array[index_pivot+1:])
 
 
-test = [random.randrange(10,10000) for _ in range(9000)]
+
+
+
+
+
+test_array = [random.randrange(10,10000) for _ in range(9000)]
+
 start_1 = time.time()
 print(start_1)
-print(quicksort(test))
+a = quicksort(test_array)
 print(time.time()-start_1)
+
+start_2 = time.time()
+print(start_2)
+b = bubblesort(test_array)
+print(time.time()-start_2)
+
+if a == b:
+    print('Yes')
